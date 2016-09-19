659e25132548bca29fa0f8c0639fa24d4c8bb6e7
QuangRau <lethanhquang910@gmail.com>
2016-09-19 17:54:28 +0700

    Add logs

--- src/index.js
+++ src/index.js
@@ -3,14 +3,3 @@ import { render } from 'react-dom';
 import App from './components/App';
 
 render(<App />, document.querySelector("#app"));
-
-
-// Logs
-console.log("%c No! Xin đừng hack em ạ :(", "background: #222; color: red; font-size: 3em");
-if (console.groupCollapsed) {
-  console.groupCollapsed("Trong trường hợp mọi người không đến dự được thì vui lòng click vào đây: ");
-  console.log("Le Thanh Quang");
-  console.log("185080449");
-  console.log("ACB Nam Sài Gòn");
-  console.groupEnd();
-}
